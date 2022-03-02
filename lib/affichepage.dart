21b6869a09f446bffb7a6c7fd268a8712e2e28bf
Augustin <educ.lecomte@gmail.com>
2022-03-02 13:35:42 +0100

    First

---
+++ lib/affichepage.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+class AffichePage extends StatefulWidget {
+  const AffichePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<AffichePage> createState() => _AffichePageState();
+}
+
+class _AffichePageState extends State<AffichePage> {
+  Map<String, dynamic> dataMap = new Map();
+  bool recupDataBool = false;
+  int id = 0;
+
+  void recupData() async {
+    await recupDataJson();
+    if (mounted) {
+      setState(() {
+        recupDataJson;
+      });
+    }
+  }
+
+  Future<void> recupDataJson() async {
+    String url = "https://pokeapi.co/api/v2/pokemon/" + this.id.toString();
+    var reponse = await http.get(Uri.parse(url));
+    if (reponse.statusCode == 200) {
+      dataMap = convert.jsonDecode(reponse.body);
+      recupDataBool = true;
+    }
+  }
+
+  Widget afficheData() {
+    Column contenu = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.empty(growable: true),
+    );
+
+    Map<String, dynamic> spritesData = dataMap['sprites'];
+    contenu.children.add(Image.network(spritesData['front_default'].toString()));
+    contenu.children.add(Image.network(spritesData['other']['official-artwork']['front_default'].toString()));
+
+    List<dynamic> formsData = dataMap['forms'];
+    contenu.children.add(Text("Name: " + formsData[0]['name'].toString()));
+    contenu.children.add(Text("Height: " + dataMap['height'].toString()));
+    contenu.children.add(Text("Weight: " + dataMap['weight'].toString()));
+
+    recupDataBool = false;
+
+    return contenu;
+  }
+
+  Widget attente() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const <Widget>[
+        Text('En attente des données', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+        CircularProgressIndicator(),
+      ],
+    );
+  }
+/*   @override
+  void dispose() {
+    super.dispose();
+  } */
+
+  @override
+  Widget build(BuildContext context) {
+    id = int.parse(ModalRoute.of(context)?.settings.arguments as String);
+    if (!recupDataBool) {
+      recupData();
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: recupDataBool ? afficheData() : attente(),
+      ),
+    );
+  }
+}
---
+++ lib/myhomepage.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
+  String _valeurSaisie = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(labelText: "N° du pokémon", hintText: "Saisir l'id d'un Pokémon"),
+                //initialValue: "9782841774470",
+                inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'[0-9]')), LengthLimitingTextInputFormatter(3)],
+                // The validator receives the text that the user has entered.
+                validator: (valeur) {
+                  if (valeur == null || valeur.isEmpty || valeur.length > 151) {
+                    return 'N° de pokémon non valide !';
+                  } else {
+                    _valeurSaisie = valeur;
+                  }
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Validate returns true if the form is valid, or false otherwise.
+                    if (_formKey.currentState!.validate()) {
+                      /* ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(_valeurSaisie),
+                        ),
+                      ); */
+                      Navigator.pushNamed(context, '/affiche', arguments: _valeurSaisie);
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
